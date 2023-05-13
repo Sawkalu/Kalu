ab0e963f734f4220e0fdeed041167bfed8bcc182
Sawkalu <122724320+Sawkalu@users.noreply.github.com>
2023-05-14 06:15:48 +0700

    Initialize auto-bump-mdc-deps-14.0.0-canary.4b5391f8b.0

---
+++ packages/tab/mwc-tab.ts
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+
+import {customElement} from 'lit/decorators.js';
+
+import {TabBase} from './mwc-tab-base';
+import {styles} from './mwc-tab.css';
+
+export {TabInteractionEventDetail} from './mwc-tab-base';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'mwc-tab': Tab;
+  }
+}
+
+@customElement('mwc-tab')
+export class Tab extends TabBase {
+  static override styles = [styles];
+}
