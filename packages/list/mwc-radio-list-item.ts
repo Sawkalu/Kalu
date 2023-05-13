b64ae6ca2927120ac8fda3a479a45820df68721f
Sawkalu <122724320+Sawkalu@users.noreply.github.com>
2023-05-14 06:18:32 +0700

    Initialize auto-bump-mdc-deps-14.0.0-canary.8c4da223a.0

---
+++ packages/list/mwc-radio-list-item.ts
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+
+
+import {customElement} from 'lit/decorators';
+
+import {styles as controlStyle} from './mwc-control-list-item.css';
+import {styles} from './mwc-list-item.css';
+import {RadioListItemBase} from './mwc-radio-list-item-base';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'mwc-radio-list-item': RadioListItem;
+  }
+}
+
+@customElement('mwc-radio-list-item')
+export class RadioListItem extends RadioListItemBase {
+  static override styles = [styles, controlStyle];
+}
