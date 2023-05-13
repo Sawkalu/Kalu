559b1919a6e00509d43d7f4f627340432d950c79
Sawkalu <122724320+Sawkalu@users.noreply.github.com>
2023-05-14 06:12:25 +0700

    Initialize outline-icons

---
+++ packages/icon/src/mwc-icon.ts
@@ -0,0 +1,48 @@
+/**
+@license
+Copyright 2018 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import {LitElement, html, property, classMap, customElement} from '@material/mwc-base/base-element';
+import {style} from './mwc-icon-host-css.js';
+import './mwc-icon-font.js';
+
+@customElement('mwc-icon' as any)
+export class Icon extends LitElement {
+
+  static styles = style;
+
+  @property()
+  theme = '';
+
+  render() {
+    const classes = {
+      'mwc-icon__outlined': this.theme === 'outlined',
+      'mwc-icon__round': this.theme === 'round',
+      'mwc-icon__two_tone': this.theme === 'two tone',
+      'mwc-icon__sharp': this.theme === 'sharp',
+    };
+    return html`<i
+        class="${classMap(classes)}">
+      <slot></slot>
+    </i>`;
+  }
+
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'mwc-icon': Icon;
+  }
+}
