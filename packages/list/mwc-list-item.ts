cf76fbcdaf2d507e9555c876c56769f7fedbeee3
Sawkalu <122724320+Sawkalu@users.noreply.github.com>
2023-05-14 06:17:27 +0700

    Initialize auto-format-action-1a28457

---
+++ packages/list/mwc-list-item.ts
@@ -0,0 +1,27 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+
+
+import {customElement} from 'lit/decorators.js';
+
+import {ListItemBase} from './mwc-list-item-base';
+import {styles} from './mwc-list-item.css';
+
+export {GraphicType, RequestSelectedDetail} from './mwc-list-item-base';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'mwc-list-item': ListItem;
+  }
+}
+
+@customElement('mwc-list-item')
+export class ListItem extends ListItemBase {
+  static override styles = [styles];
+}
