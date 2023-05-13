9f14e0108fd9fdcdfe9fcc18ddb9da626f2c9b06
Sawkalu <122724320+Sawkalu@users.noreply.github.com>
2023-05-14 06:12:58 +0700

    Initialize cl/384742100

---
+++ packages/icon-button-toggle/mwc-icon-button-toggle.ts
@@ -0,0 +1,25 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+
+
+import {styles} from '@material/mwc-icon-button/mwc-icon-button.css';
+import {customElement} from 'lit-element';
+
+import {IconButtonToggleBase} from './mwc-icon-button-toggle-base';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'mwc-icon-button-toggle': IconButtonToggle;
+  }
+}
+
+@customElement('mwc-icon-button-toggle')
+export class IconButtonToggle extends IconButtonToggleBase {
+  static styles = [styles];
+}
---
+++ packages/ripple/mwc-ripple.ts
@@ -0,0 +1,25 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+
+import {customElement} from 'lit-element';
+
+import {RippleBase} from './mwc-ripple-base';
+import {styles} from './mwc-ripple.css';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'mwc-ripple': Ripple;
+  }
+}
+
+/** @soyCompatible */
+@customElement('mwc-ripple')
+export class Ripple extends RippleBase {
+  static styles = [styles];
+}
