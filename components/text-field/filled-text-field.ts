8791f17869b77ae73b1587486b55ae425b9436a8
Sawkalu <122724320+Sawkalu@users.noreply.github.com>
2023-05-14 06:17:36 +0700

    Initialize auto-format-action-13761f8

---
+++ components/text-field/filled-text-field.ts
@@ -0,0 +1,51 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import '../field/filled-field';
+
+import {html, TemplateResult} from 'lit';
+import {customElement, queryAsync} from 'lit/decorators';
+
+import {MdFilledField} from '../field/filled-field';
+
+import {styles as filledStyles} from './lib/filled-styles.css';
+import {FilledTextField} from './lib/filled-text-field';
+import {styles as sharedStyles} from './lib/shared-styles.css';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'md-filled-text-field': MdFilledTextField;
+  }
+}
+
+/**
+ * @soyCompatible
+ * @final
+ * @suppress {visibility}
+ */
+@customElement('md-filled-text-field')
+export class MdFilledTextField extends FilledTextField {
+  static override styles = [sharedStyles, filledStyles];
+
+  @queryAsync('md-filled-field')
+  protected readonly field!: Promise<MdFilledField>;
+
+  /** @soyTemplate */
+  protected override renderField(): TemplateResult {
+    return html`
+      <md-filled-field
+        id=${this.fieldID}
+        .disabled=${this.disabled}
+        .error=${this.error}
+        .label=${this.label}
+        .populated=${Boolean(this.value)}
+        .required=${this.required}
+      >
+        ${this.renderFieldContent()}
+      </md-filled-field>
+    `;
+  }
+}
