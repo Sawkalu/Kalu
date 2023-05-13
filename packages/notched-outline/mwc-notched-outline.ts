97a6db95f527f60500c9338c683d30eb2fefac03
Sawkalu <122724320+Sawkalu@users.noreply.github.com>
2023-05-14 06:13:23 +0700

    Initialize cl/390428258

---
+++ packages/notched-outline/mwc-notched-outline.ts
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+
+import {customElement} from 'lit-element';
+
+import {NotchedOutlineBase} from './mwc-notched-outline-base';
+import {styles} from './mwc-notched-outline.css';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'mwc-notched-outline': NotchedOutline;
+  }
+}
+
+@customElement('mwc-notched-outline')
+export class NotchedOutline extends NotchedOutlineBase {
+  static override styles = [styles];
+}
