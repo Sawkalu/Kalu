f2ee3bf7f0e1dfe36d3e9f1c718d7616bff2617e
Sawkalu <122724320+Sawkalu@users.noreply.github.com>
2023-05-14 06:13:03 +0700

    Initialize allan-chen-patch-1

---
+++ packages/icon/mwc-icon.ts
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+
+import {customElement, html, LitElement, TemplateResult} from 'lit-element';
+
+import {styles} from './mwc-icon-host.css';
+
+/** @soyCompatible */
+@customElement('mwc-icon')
+export class Icon extends LitElement {
+  static styles = [styles];
+
+  /** @soyTemplate */
+  protected render(): TemplateResult {
+    return html`<slot></slot>`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'mwc-icon': Icon;
+  }
+}
---
+++ packages/notched-outline/mwc-notched-outline-base.ts
@@ -0,0 +1,69 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+
+import {BaseElement} from '@material/mwc-base/base-element';
+import {MDCNotchedOutlineAdapter} from '@material/notched-outline/adapter';
+import {MDCNotchedOutlineFoundation} from '@material/notched-outline/foundation';
+import {html, property, query} from 'lit-element';
+import {classMap} from 'lit-html/directives/class-map';
+
+export class NotchedOutlineBase extends BaseElement {
+  @query('.mdc-notched-outline') protected mdcRoot!: HTMLElement;
+  protected mdcFoundation!: MDCNotchedOutlineFoundation;
+
+  protected readonly mdcFoundationClass = MDCNotchedOutlineFoundation;
+
+  @property({type: Number}) width = 0;
+
+  @property({type: Boolean, reflect: true}) open = false;
+
+  protected lastOpen = this.open;
+
+  @query('.mdc-notched-outline__notch') protected notchElement!: HTMLDivElement;
+
+  protected createAdapter(): MDCNotchedOutlineAdapter {
+    return {
+      addClass: (className) => this.mdcRoot.classList.add(className),
+      removeClass: (className) => this.mdcRoot.classList.remove(className),
+      setNotchWidthProperty: (width) =>
+          this.notchElement.style.setProperty('width', `${width}px`),
+      removeNotchWidthProperty: () =>
+          this.notchElement.style.removeProperty('width'),
+    };
+  }
+
+  protected openOrClose(shouldOpen: boolean, width?: number) {
+    if (!this.mdcFoundation) {
+      return;
+    }
+
+    if (shouldOpen && width !== undefined) {
+      this.mdcFoundation.notch(width);
+    } else {
+      this.mdcFoundation.closeNotch();
+    }
+  }
+
+  render() {
+    this.openOrClose(this.open, this.width);
+
+    const classes = classMap({
+      'mdc-notched-outline--notched': this.open,
+    });
+
+    return html`
+      <span class="mdc-notched-outline ${classes}">
+        <span class="mdc-notched-outline__leading"></span>
+        <span class="mdc-notched-outline__notch">
+          <slot></slot>
+        </span>
+        <span class="mdc-notched-outline__trailing"></span>
+      </span>`;
+  }
+}
