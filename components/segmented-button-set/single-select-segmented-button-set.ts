eb99f46a1c4330782064e51981cac7d7786af733
Sawkalu <122724320+Sawkalu@users.noreply.github.com>
2023-05-14 06:18:30 +0700

    Initialize cl/442096057

---
+++ components/segmented-button-set/single-select-segmented-button-set.ts
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {customElement} from 'lit/decorators';
+
+import {SingleSelectSegmentedButtonSet} from './lib/segmented-button-set';
+import {styles} from './lib/shared-styles.css';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'md-single-select-segmented-button-set': MdSingleSelectSegmentedButtonSet;
+  }
+}
+
+/**
+ * MdSingleSelectSegmentedButtonSet is the custom element for the single-select
+ * behavior of Material Design segmented buttons.
+ */
+@customElement('md-single-select-segmented-button-set')
+export class MdSingleSelectSegmentedButtonSet extends
+    SingleSelectSegmentedButtonSet {
+  static override styles = [styles];
+}
---
+++ components/text-field/filled-text-field.ts
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import '../field/filled-field';
+
+import {html, TemplateResult} from 'lit';
+import {customElement} from 'lit/decorators';
+
+import {styles as filledStyles} from './lib/filled-styles.css';
+import {FilledTextField} from './lib/filled-text-field';
+import {styles as sharedStyles} from './lib/shared-styles.css';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'md-filled-text-field': MdFilledTextField;
+  }
+}
+
+/**
+ * @soyCompatible
+ * @final
+ * @suppress {visibility}
+ */
+@customElement('md-filled-text-field')
+export class MdFilledTextField extends FilledTextField {
+  static override styles = [sharedStyles, filledStyles];
+
+  /** @soyTemplate */
+  protected override renderField(): TemplateResult {
+    return html`
+      <md-filled-field
+        class="md3-text-field__field"
+        id=${this.fieldID}
+        .disabled=${this.disabled}
+        .error=${this.error}
+        .label=${this.label}
+        .populated=${Boolean(this.value)}
+        .required=${this.required}
+      >
+        ${this.renderFieldContent()}
+      </md-filled-field>
+    `;
+  }
+}
