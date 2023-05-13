ad83097385639de9058fb81f125b048f7b512bdb
Sawkalu <122724320+Sawkalu@users.noreply.github.com>
2023-05-14 06:18:00 +0700

    Initialize cl/434728573

---
+++ components/fab/lib/fab.ts
@@ -0,0 +1,23 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {ClassInfo} from 'lit/directives/class-map';
+
+import {FabShared} from './fab-shared';
+
+/**
+ * Fab Extended Base class logic and template definition
+ * @soyCompatible
+ */
+export class Fab extends FabShared {
+  /** @soyTemplate */
+  protected override getRootClasses(): ClassInfo {
+    return {
+      ...super.getRootClasses(),
+      'md3-fab--regular': true,
+    };
+  }
+}
