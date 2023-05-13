5e35f1cdd0006a7107d445981bf48ddd95ac80ef
Sawkalu <122724320+Sawkalu@users.noreply.github.com>
2023-05-14 06:13:21 +0700

    Initialize auto-bump-mdc-deps-13.0.0-canary.c16fe03ce.0

---
+++ packages/textarea/mwc-textarea.ts
@@ -0,0 +1,28 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+
+
+import {styles as textfieldStyles} from '@material/mwc-textfield/mwc-textfield.css';
+import {customElement} from 'lit-element';
+import {TextAreaBase, TextAreaCharCounter, TextFieldInputMode, TextFieldType} from './mwc-textarea-base';
+import {styles as textareaStyles} from './mwc-textarea.css';
+
+export {TextAreaCharCounter, TextFieldInputMode, TextFieldType};
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'mwc-textarea': TextArea;
+  }
+}
+
+/** @soyCompatible */
+@customElement('mwc-textarea')
+export class TextArea extends TextAreaBase {
+  static override styles = [textfieldStyles, textareaStyles];
+}
