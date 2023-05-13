9a71acf06305989a8f181c29092a7936b6bd9d73
Sawkalu <122724320+Sawkalu@users.noreply.github.com>
2023-05-14 06:13:05 +0700

    Initialize cl/390437391

---
+++ packages/fab/mwc-fab.ts
@@ -0,0 +1,25 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+
+import {customElement} from 'lit-element';
+
+import {FabBase} from './mwc-fab-base';
+import {styles} from './mwc-fab.css';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'mwc-fab': Fab;
+  }
+}
+
+/** @soyCompatible */
+@customElement('mwc-fab')
+export class Fab extends FabBase {
+  static styles = [styles];
+}
